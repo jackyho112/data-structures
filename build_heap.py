d62359b310a352b1a9047628240ba8c17f111719
jackyho112 <jackyho112dev@gmail.com>
2017-09-02 20:43:41 -0700

    Finish algo

--- build_heap.py
+++ build_heap.py
@@ -15,19 +15,41 @@ class HeapBuilder:
     for swap in self._swaps:
       print(swap[0], swap[1])
 
+  def left_child_index(self, index):
+    left_child_index = 2 * index + 1
+
+    if left_child_index >= len(self._data):
+        return False
+
+    return left_child_index
+
+  def right_child_index(self, index):
+    right_child_index = 2 * index + 2
+
+    if right_child_index >= len(self._data):
+        return False
+
+    return right_child_index
+
+  def sift_down(self, i):
+    index = i
+    left_child_index = self.left_child_index(i)
+    right_child_index = self.right_child_index(i)
+
+    if right_child_index and self._data[right_child_index] < self._data[index]:
+      index = right_child_index
+
+    if left_child_index and self._data[left_child_index] < self._data[index]:
+      index = left_child_index
+
+    if i != index:
+      self._swaps.append((i, index))
+      self._data[i], self._data[index] = self._data[index], self._data[i]
+      self.sift_down(index)
+
   def GenerateSwaps(self):
-    # The following naive implementation just sorts 
-    # the given sequence using selection sort algorithm
-    # and saves the resulting sequence of swaps.
-    # This turns the given array into a heap, 
-    # but in the worst case gives a quadratic number of swaps.
-    #
-    # TODO: replace by a more efficient implementation
-    for i in range(len(self._data)):
-      for j in range(i + 1, len(self._data)):
-        if self._data[i] > self._data[j]:
-          self._swaps.append((i, j))
-          self._data[i], self._data[j] = self._data[j], self._data[i]
+    for i in range(len(self._data) // 2, -1, -1):
+        self.sift_down(i)
 
   def Solve(self):
     self.ReadData()
---
+++ merging_tables.py
@@ -0,0 +1,49 @@
+# python3
+
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+lines = list(map(int, sys.stdin.readline().split()))
+rank = [0] * n
+parents = list(range(0, n))
+answers = []
+
+def getParent(table):
+    balance_parents = []
+
+    index = table
+    while index != parents[index]:
+        balance_parents.append(parents[index])
+        index = parents[index]
+
+    for i in balance_parents:
+        parents[i] = index
+
+    # find parent and compress path
+    return index
+
+def merge(destination, source):
+    realDestination, realSource = getParent(destination), getParent(source)
+
+    if realDestination == realSource:
+        return False
+
+    if rank[realSource] >= rank[realDestination]:
+        parents[realSource] = realDestination
+    else:
+        parents[realDestination] = realSource
+        if rank[realSource] == rank[realDestination]:
+            rank[realSource] += 1
+
+    lines[realDestination] += lines[realSource]
+    lines[realSource] = 0
+
+    if answers[-1] < lines[realDestination]:
+        answers.append(lines[realDestination])
+
+    return True
+
+for i in range(m):
+    destination, source = map(int, sys.stdin.readline().split())
+    merge(destination - 1, source - 1)
+    print(answers[-1])
